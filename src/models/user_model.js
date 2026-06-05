eaa4199d125323113691079a71293cb97fe91789
Esppiiiii123 <alvaroespinar7@gmail.com>
2026-06-05 19:44:35 +0200

    feat: modelo User en CommonJS

---
+++ src/models/user_model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, "El email es obligatorio"],
+        unique: true, // Crea un índice único en Atlas para evitar correos duplicados
+        lowercase: true, // Fuerza a que se guarde siempre en minúsculas
+        trim: true
+    },
+    password: {
+        type: String,
+        required: [true, "La contraseña es obligatoria"]
+    }
+}, { 
+    timestamps: true // Nos da 'createdAt' y 'updatedAt' gratis
+});
+
+// Creamos el modelo intermediario
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
